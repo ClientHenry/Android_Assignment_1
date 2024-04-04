34af0f7415bc68658da4c4931fd06de0a425f7f1
ClientHenry <wanghao0628@hotmail.com>
2024-04-05 02:13:24 +1300

    1

--- app/src/main/java/com/example/assignment_1/Deal_Detail_Page.java
+++ app/src/main/java/com/example/assignment_1/Deal_Detail_Page.java
@@ -5,7 +5,9 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Deal_Detail_Page extends AppCompatActivity {
     private ViewPager mViewPager;
@@ -24,6 +26,15 @@ public class Deal_Detail_Page extends AppCompatActivity {
         lm.setOrientation(RecyclerView.HORIZONTAL);
         rc_service.setLayoutManager(lm);
         rc_service.setAdapter(new Deal_Detail_Service_Recycler_Adapter());
+
+        Button btn_deal_detail = findViewById(R.id.btn_deal_detail);
+        btn_deal_detail.setOnClickListener(v -> {
+
+            Intent intent = new Intent(Deal_Detail_Page.this, Login_Page.class);
+            startActivity(intent);
+        });
+
+
 
 
 
